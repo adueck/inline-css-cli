9ea7d4234000d9b095e62141452066ab17e6d4c2
Adam Dueck <adueck@users.noreply.github.com>
2019-04-26 00:22:28 +0400

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var inlineCss = require('inline-css');
 var getStdin = require('get-stdin');
 
 getStdin().then(stdin => {
-	inlineCss(stdin, { url: 'https://example.com' }).then(console.log);
+	inlineCss(stdin, { url: ' ' }).then(console.log);
 });
 
